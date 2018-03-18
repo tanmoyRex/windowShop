463498dea580eea869f15e8771b6dbc54fb339a7
tanmoyRex <tanmoyRex@users.noreply.github.com>
2018-03-18 20:58:03 +0600

    added calculate button to keyboard

--- windowShop/windowShop/Controller/MainVC.swift
+++ windowShop/windowShop/Controller/MainVC.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var wageTxt: CurrencyTextField!
+    @IBOutlet weak var priceTxt: CurrencyTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
         // Do any additional setup after loading the view, typically from a nib.
+        calcBtn.backgroundColor = #colorLiteral(red: 0.9750951777, green: 0.5656782951, blue: 0.01254588252, alpha: 1)
+        calcBtn.setTitle("calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+//        calcBtn.titleLabel?.text = "calculate"   <<<------ this does not work for button but works for lebel
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+    }
+    
+    @objc func calculate() {
+        print("we got here")
+        
     }
 
     override func didReceiveMemoryWarning() {
--- windowShop/windowShop/Views/CurrencyTextField.swift
+++ windowShop/windowShop/Views/CurrencyTextField.swift
@@ -8,21 +8,30 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTextField: UITextField {
+    
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+       customizeView()
+    }
+    
+    func customizeView() {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)    //<<<---- color literal
         // in the past <<<------
-       // backgroundColor = UIColor(red: <#T##CGFloat#>, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: <#T##CGFloat#>)
+        // backgroundColor = UIColor(red: <#T##CGFloat#>, green: <#T##CGFloat#>, blue: <#T##CGFloat#>, alpha: <#T##CGFloat#>)
         layer.cornerRadius = 5.0
         textAlignment = .center
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) ]) //UIColor.white])
-                    attributedPlaceholder = place
-                    textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         }
-        
     }
 }
         
